ebe562bfb2f7eb22b582028c1f55bf037b3def43
RyanCountryman <58121707+RyanCountryman@users.noreply.github.com>
2024-11-30 12:24:33 -0800

    PPU Register Declarations
    Register Declarations and Initialization

--- PPU.h
+++ PPU.h
@@ -1,14 +1,29 @@
 /*
     Created by Ryan Countryman on 11/17/2024
-    Last Updated: 11/17/2024
+    Last Updated: 11/30/2024
 */
 #ifndef PPU_H
 #define PPU_H
 
+#include <cstdint>
+
 class PPU
 {
 public:
-    void respTest();
+	uint8_t PPUCTRL = 0x00; //General Controller (Write)
+	uint8_t PPUMASK = 0x00; //Rendering Controller (Write)
+	uint8_t PPUSTATUS = 0x00; //Rendering Status (Read)
+	uint8_t OAMADDR = 0x00; //Sprite RAM Address (Write)
+	uint8_t OAMDATA = 0x00; //Sprite Data(Read / Write)
+	uint8_t PPUSCROLL = 0x00; //X-Y Coords for Scroll (Write) WRITE AFTER PPUADDR
+	uint8_t PPUADDR = 0x00; //VRAM Address (Write) Communication with CPU
+	uint8_t PPUDATA = 0x00; //VRAM Data (Read / Write) 
+	uint8_t OAMDMA = 0x00; //Sprite DMA (Write) Suspend CPU to begin DMA
+    
+	PPU(){
+	}
+
+	void respTest();
 };
 
 #endif
