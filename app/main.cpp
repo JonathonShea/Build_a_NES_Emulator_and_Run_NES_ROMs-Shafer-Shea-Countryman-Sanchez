68df1933f6034395db48e92e3a8b541fd00b43af
Jonathon Shea <sheajonathon0@gmail.com>
2024-12-01 15:22:20 -0500

    Moved OpenFileDialog to Utilities Namespace and file.

--- app/main.cpp
+++ app/main.cpp
@@ -4,12 +4,11 @@
 #include <iostream>
 #include <fstream>
 #include <string> 
-#include <shobjidl_core.h>
 #include <Cartridge.h>
+#include <Utilities.h>
 
 static constexpr std::array<uint8_t, 4> magicNumbers = { 0x4E, 0x45, 0x53, 0x1A }; // NES<EOF> magic numbers to identify a NES ROM file
 
-std::string OpenFileDialog();
 
 int main(int argc, const char * argv[]){
 	Clock clock;
@@ -20,7 +19,7 @@ int main(int argc, const char * argv[]){
         filePath = argv[1];
     }
     else{
-        filePath = OpenFileDialog();
+        filePath = Utilities::OpenFileDialog();
     }
 	romFile.open(filePath, std::ios::binary);
 	if (romFile.is_open()) {
@@ -44,39 +43,3 @@ int main(int argc, const char * argv[]){
 }
 
 
-// Opens a windows file open dialog based on https://learn.microsoft.com/en-us/windows/win32/shell/common-file-dialog
-std::string OpenFileDialog() {
-    HRESULT hr;
-    std::string filePath;
-    IFileOpenDialog* pFileOpen;
-
-    // Initialize COM library
-    hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE);
-    if (SUCCEEDED(hr)) {
-        // Create the FileOpenDialog object
-        hr = CoCreateInstance(CLSID_FileOpenDialog, NULL, CLSCTX_ALL, IID_IFileOpenDialog, reinterpret_cast<void**>(&pFileOpen));
-        if (SUCCEEDED(hr)) {
-            // Show the Open dialog box
-            hr = pFileOpen->Show(NULL);
-            if (SUCCEEDED(hr)) {
-                // Get the file name from the dialog box
-                IShellItem* pItem;
-                hr = pFileOpen->GetResult(&pItem);
-                if (SUCCEEDED(hr)) {
-                    PWSTR pszFilePath;
-                    hr = pItem->GetDisplayName(SIGDN_FILESYSPATH, &pszFilePath);
-                    if (SUCCEEDED(hr)) {
-                        // Convert the file path to a string
-                        std::wstring ws(pszFilePath);
-                        filePath = std::string(ws.begin(), ws.end());
-                        CoTaskMemFree(pszFilePath);
-                    }
-                    pItem->Release();
-                }
-            }
-            pFileOpen->Release();
-        }
-        CoUninitialize();
-    }
-    return filePath;
-}
