daca34921a07a42b6303830ccf4df9685688002f
Gwyndolyn <gwyndolyn.shafer@gmail.com>
2024-12-01 12:32:11 -0800

    added empty apu class

---
+++ src/apu.h
@@ -0,0 +1,20 @@
+/*
+    Created by Gwyn Shafer on 11/28/2024
+    Last Updated: 11/28/2024
+*/
+#ifndef APU_H
+#define APU_H
+
+#include <cstdint>
+
+class APU
+{
+public:
+	APU(){
+		
+	}
+
+	void test();
+};
+
+#endif
