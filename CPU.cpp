fa07c6346228909c7db2e8da224494783982366b
Gwyndolyn <gwyndolyn.shafer@gmail.com>
2025-01-21 15:20:16 -0800

    added shift opcodes and rough memory implementation

--- CPU.cpp
+++ CPU.cpp
@@ -1,5 +1,5 @@
-// Created by Abel Sanchez om 11/17/2024
-// Last updated: 12/1/2024
+// Created by Abel Sanchez on 11/17/2024
+// Last updated: 1/21/2025
 
 #include "CPU.h"
 #include <iostream>
@@ -7,6 +7,126 @@
 void CPU::respTest()
 {
     std::cout << "The CPU says hi! Say hi back or else..." << std::endl;
+}
+
+CPU::CPU() : memory(65536, 0) // Initialize 64KB of memory
+{
+}
+
+
+uint8_t CPU::read(uint16_t addr)
+{
+    return memory[addr];
+}
+
+void CPU::write(uint16_t addr, uint8_t data)
+{
+    memory[addr] = data;
+}
+
+// shift instructions
+void CPU::ASL(uint16_t addr)
+{
+    uint8_t value = read(addr);
+    setCarryFlag(value & 0x80);
+    value <<= 1;
+    write(addr, value);
+    setZeroFlag(value == 0);
+    setNegativeFlag(value & 0x80);
+}
+
+void CPU::LSR(uint16_t addr)
+{
+    uint8_t value = read(addr);
+    setCarryFlag(value & 0x01); 
+    value >>= 1;            
+    write(addr, value);
+    setZeroFlag(value == 0); 
+    setNegativeFlag(0);        
+}
+
+void CPU::ROL(uint16_t addr)
+{
+    uint8_t value = read(addr);
+    bool carry = getCarryFlag();
+    setCarryFlag(value & 0x80);
+    value = (value << 1) | carry;
+    write(addr, value);
+    setZeroFlag(value == 0);
+    setNegativeFlag(value & 0x80);
+}
+
+void CPU::ROR(uint16_t addr)
+{
+    uint8_t value = read(addr);
+    bool carry = getCarryFlag();
+    setCarryFlag(value & 0x01);
+    value = (value >> 1) | (carry << 7);
+    write(addr, value);
+    setZeroFlag(value == 0);
+    setNegativeFlag(value & 0x80);
+}
+
+// flags
+void CPU::setCarryFlag(bool value)
+{
+    if (value)
+        status |= 0x01;
+    else
+        status &= ~0x01;
+}
+
+bool CPU::getCarryFlag()
+{
+    return status & 0x01;
+}
+
+void CPU::setZeroFlag(bool value)
+{
+    if (value)
+        status |= 0x02;
+    else
+        status &= ~0x02;
+}
+
+void CPU::setInterruptDisableFlag(bool value)
+{
+    if (value)
+        status |= 0x04;
+    else
+        status &= ~0x04;
+}
+
+void CPU::setDecimalModeFlag(bool value)
+{
+    if (value)
+        status |= 0x08;
+    else
+        status &= ~0x08;
+}
+
+void CPU::setBreakCommandFlag(bool value)
+{
+    if (value)
+        status |= 0x10;
+    else
+        status &= ~0x10;
+}
+
+void CPU::setOverflowFlag(bool value)
+{
+    if (value)
+        status |= 0x40;
+    else
+        status &= ~0x40;
+}
+
+void CPU::setNegativeFlag(bool value)
+{
+    if (value)
+        status |= 0x80;
+    else
+        status &= ~0x80;
 }
 
 int main()
--- CPU.h
+++ CPU.h
@@ -1,10 +1,11 @@
 // Created by Abel Sanchez om 11/17/2024
-// Last updated: 12/1/2024
+// Last updated: 1/21/2025
 
 #ifndef CPU_H
 #define CPU_H
 
 #include <cstdint>
+#include <vector>
 
 class CPU
 {
@@ -13,13 +14,31 @@ public:
 	uint8_t x = 0x00; // 8-bit general purpose register
 	uint8_t y = 0x00; // 8-bit general purpose register
 	uint8_t stack_pointer = 0x0000; // 8-bit register that contains lower 8 bits of stack
-	uint16_t = program_counter = 0x00; // 16-bit register that contains a pointer to the next instruction
+	uint16_t program_counter = 0x00; // 16-bit register that contains a pointer to the next instruction
 	uint8_t status = 0x00; // 8-bit register that contains status flags
 
-	CPU(){
-	}
+	CPU();
 
 	void respTest();
+	uint8_t read(uint16_t addr);
+	void write(uint16_t addr, uint8_t data);
+	void ASL(uint16_t addr);
+	void LSR(uint16_t addr);
+	void ROL(uint16_t addr);
+	void ROR(uint16_t addr);
+
+
+private:
+	std::vector<uint8_t> memory;
+
+	void setCarryFlag(bool value);
+    bool getCarryFlag();
+    void setZeroFlag(bool value);
+    void setInterruptDisableFlag(bool value);
+    void setDecimalModeFlag(bool value);
+    void setBreakCommandFlag(bool value);
+    void setOverflowFlag(bool value);
+    void setNegativeFlag(bool value);
 };
 
 #endif
